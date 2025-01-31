7ec31bea5ce3a256d5b3e76fe0dd0f310d19c2b9
urvi <urvisurti897@gmail.com>
2025-01-31 18:19:26 -0500

    Initializing the variables

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@
     //Task 1 - Total money spent by Carlo
     internal class Program
     {
+        //Initilizing the variables
+        int CalgaryPrice = 1350;
+        int VancouverPrice = 1500;
+        int MontrealPrice = 575;
+
         static void Main(string[] args)
         {
             
