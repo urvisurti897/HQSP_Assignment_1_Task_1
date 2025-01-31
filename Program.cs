cbaec3df09bc6e7a7f8b2f33c4d3f58d990f321c
urvi <urvisurti897@gmail.com>
2025-01-31 18:12:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HQSP_Assignment_1
+{
+    //Task 1 - Total money spent by Carlo
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
