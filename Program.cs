ca642791c6795db5d300b1e4a677be66fbec82c6
urvi <urvisurti897@gmail.com>
2025-01-31 19:19:18 -0500

    Calling all methods in main method

--- Program.cs
+++ Program.cs
@@ -4,28 +4,35 @@
     internal class Program
     {
         //Initializing the variables
-        int CalgaryPrice = 1350;
-        int VancouverPrice = 1500;
-        int MontrealPrice = 575;
+        static int CalgaryPrice = 1350;
+        static int VancouverPrice = 1500;
+        static int MontrealPrice = 575;
 
         static void Main(string[] args)
         {
-            
+            int calgaryTrips = GetNumberOfTrips("Calgary");
+            int vancouverTrips = GetNumberOfTrips("Vancouver");
+            int montrealTrips = GetNumberOfTrips("Montreal");
+
+            int totalSpent = CalculateTotalMoneySpent(calgaryTrips, vancouverTrips, montrealTrips);
+            int averageSpent = CalculateAverageMoneySpent(totalSpent, calgaryTrips + vancouverTrips + montrealTrips);
+
+            DisplayResult(totalSpent, averageSpent);
         }
 
         static int GetNumberOfTrips(string destination)
         {
-            Console.Write("Enter number of trips for" + destination + " : ");
+            Console.Write("Enter number of trips for " + destination + " : ");
             return int.Parse(Console.ReadLine());
         }
 
         static int CalculateTotalMoneySpent(int calgaryTrips, int vancouverTrips, int montrealTrips)
         {
-            int ctms = (calgaryTrips * CalgaryPrice) + (vancouverTrips * VancouverPrice) + (montrealTrips * MontrealPrice)
+            int ctms = (calgaryTrips * CalgaryPrice) + (vancouverTrips * VancouverPrice) + (montrealTrips * MontrealPrice);
             return ctms;
         }
 
-        static double CalculateAverageMoneySpent(double totalSpent , int totalTrips)
+        static int CalculateAverageMoneySpent(int totalSpent , int totalTrips)
         {
             return totalTrips > 0 ? 
                    totalSpent / totalTrips : 0;
@@ -34,7 +41,7 @@
         static void DisplayResult(double totalMoneySpent, double averageSpent)
         {
             Console.WriteLine("Total money spent: $" + totalMoneySpent);
-            Console.WriteLine("Average price calaculation per trip: $" + averageSpent);
+            Console.WriteLine("Average price calculation per trip: $" + averageSpent);
         }
     }
 }
